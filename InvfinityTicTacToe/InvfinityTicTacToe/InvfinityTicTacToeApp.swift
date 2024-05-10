47670146a14d8e259394888802f1af6ade581f19
Lev <w9430@yandex.ru>
2024-05-10 19:52:05 +0300

    Initial project creation

---
+++ InvfinityTicTacToe/InvfinityTicTacToe/InvfinityTicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvfinityTicTacToeApp.swift
+//  InvfinityTicTacToe
+//
+//  Created by Лев Куликов on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct InvfinityTicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
